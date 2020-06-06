018bb2f1e34c5b181e9988f239384c40ba52c845
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-07 03:11:22 +0530

    Integrated the card count feature

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../css/Card.module.css';
 
-const Card = ({ name, numbers }) => {
-  const renderIcon = (card, number) => {
+const Card = ({ player }) => {
+  const [bricks, setBricks] = useState(0);
+  const [lumber, setLumber] = useState(0);
+  const [wool, setWool] = useState(0);
+  const [grain, setGrain] = useState(0);
+  const [ore, setOre] = useState(0);
+  console.log(player);
+  const renderIcon = (card, number, callback) => {
     return (
       <div className='item'>
         <div className='ui tiny image'>
           <img src={require(`../img/cards/${card}.svg`)} alt='brick' />
         </div>
         <div className='middle aligned content'>
-          <button className='ui button primary'>Add</button>
-          <button className='ui button negative'>Remove</button>
+          <button
+            className='ui button primary'
+            onClick={() => callback(number + 1)}
+          >
+            Add
+          </button>
+          <button
+            className='ui button negative'
+            onClick={() => callback(number - 1)}
+          >
+            Remove
+          </button>
         </div>
         <div className={styles.statistic}>
           <h2> {number}</h2>
@@ -20,22 +36,22 @@ const Card = ({ name, numbers }) => {
     );
   };
 
-  const renderIcons = (numbers) => {
+  const renderIcons = () => {
     return (
       <div className='ui items'>
-        {renderIcon('card_brick', numbers[0])}
-        {renderIcon('card_lumber', numbers[1])}
-        {renderIcon('card_wool', numbers[2])}
-        {renderIcon('card_grain', numbers[3])}
-        {renderIcon('card_ore', numbers[4])}
+        {renderIcon('card_brick', bricks, setBricks)}
+        {renderIcon('card_lumber', lumber, setLumber)}
+        {renderIcon('card_wool', wool, setWool)}
+        {renderIcon('card_grain', grain, setGrain)}
+        {renderIcon('card_ore', ore, setOre)}
       </div>
     );
   };
 
   return (
     <div className={styles.card}>
-      <h2 className={styles.playerName}>{name}</h2>
-      {renderIcons(numbers)}
+      <h2 className={styles.playerName}>{player.playerName}</h2>
+      {renderIcons()}
     </div>
   );
 };
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Card from './Card';
 
+import Card from './Card';
 import styles from '../css/Card.module.css';
 
 const Cards = ({ players }) => {
+  const renderCards = players.map((player, index) => {
+    return <Card key={player.playerName} player={players[index]} />;
+  });
+
   return (
     <div className='ui container'>
-      <div className={styles.cardsContainer}>
-        <Card name={'Ritesh'} numbers={[1, 2, 3, 4, 5]} />
-        <Card name={'Ritesh'} numbers={[1, 2, 3, 4, 5]} />
-        <Card name={'Ritesh'} numbers={[1, 2, 3, 4, 5]} />
-      </div>
+      <div className={styles.cardsContainer}>{renderCards}</div>
     </div>
   );
 };
