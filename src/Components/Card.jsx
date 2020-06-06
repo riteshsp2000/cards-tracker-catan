92e285e34c2fb208b0e0ca0f3df495cc5cad7131
Ritesh Patil <riteshsp2000@gmail.com>
2020-06-07 03:57:15 +0530

    Added the settlements option

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,7 +8,7 @@ const Card = ({ player }) => {
   const [wool, setWool] = useState(0);
   const [grain, setGrain] = useState(0);
   const [ore, setOre] = useState(0);
-  console.log(player);
+
   const renderIcon = (card, number, callback) => {
     return (
       <div className='item'>
@@ -48,10 +48,194 @@ const Card = ({ player }) => {
     );
   };
 
+  const renderTitleIcon = () => {
+    switch (player.color) {
+      case 'Red':
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_red.svg')}
+            alt='Red'
+          />
+        );
+      case 'Blue':
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_blue.svg')}
+            alt='Blue'
+          />
+        );
+      case 'Orange':
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_orange.svg')}
+            alt='Orange'
+          />
+        );
+      case 'Green':
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_green.svg')}
+            alt='Green'
+          />
+        );
+      case 'Black':
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_black.svg')}
+            alt='Black'
+          />
+        );
+      default:
+        return (
+          <img
+            className={styles.titleImage}
+            src={require('../img/icons/player_bg_settler.svg')}
+            alt='Settler'
+          />
+        );
+    }
+  };
+
+  const renderCities = () => {
+    switch (player.color) {
+      case 'Red':
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_red.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_red.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+      case 'Blue':
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_blue.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_blue.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+      case 'Orange':
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_orange.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_orange.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+      case 'Green':
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_green.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_green.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+      case 'Black':
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_black.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_black.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+      default:
+        return (
+          <div className={styles.citiesDiv}>
+            <img
+              className={styles.titleImage}
+              src={require('../img/city/city_settler.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/settlement/settlement_settler.svg')}
+              alt='Red'
+            />
+            <img
+              className={styles.titleImage}
+              src={require('../img/devcard.svg')}
+              alt='Red'
+            />
+          </div>
+        );
+    }
+  };
+
   return (
     <div className={styles.card}>
-      <h2 className={styles.playerName}>{player.playerName}</h2>
+      <div className={styles.titleDiv}>
+        {renderTitleIcon()}
+        <span>
+          <h2 className={styles.playerName}>{player.playerName}</h2>
+        </span>
+      </div>
       {renderIcons()}
+      {renderCities()}
     </div>
   );
 };
